48c59822d6bbe051994c9fe2eafa681be1d22f4f
ysyyhhh <757578166@qq.com>
2023-10-04 21:29:26 +0800

    complete prog1

--- prog1_mandelbrot_threads/mandelbrotSerial.cpp
+++ prog1_mandelbrot_threads/mandelbrotSerial.cpp
@@ -37,23 +37,23 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 static inline int mandel(float c_re, float c_im, int count)
 {
-    float z_re = c_re, z_im = c_im;
-    int i;
-    for (i = 0; i < count; ++i) {
+  float z_re = c_re, z_im = c_im;
+  int i;
+  for (i = 0; i < count; ++i)
+  {
 
-        if (z_re * z_re + z_im * z_im > 4.f)
-            break;
+    if (z_re * z_re + z_im * z_im > 4.f)
+      break;
 
-        float new_re = z_re*z_re - z_im*z_im;
-        float new_im = 2.f * z_re * z_im;
-        z_re = c_re + new_re;
-        z_im = c_im + new_im;
-    }
+    float new_re = z_re * z_re - z_im * z_im;
+    float new_im = 2.f * z_re * z_im;
+    z_re = c_re + new_re;
+    z_im = c_im + new_im;
+  }
 
-    return i;
+  return i;
 }
 
 //
@@ -74,19 +74,20 @@ void mandelbrotSerial(
     int maxIterations,
     int output[])
 {
-    float dx = (x1 - x0) / width;
-    float dy = (y1 - y0) / height;
+  float dx = (x1 - x0) / width;
+  float dy = (y1 - y0) / height;
 
-    int endRow = startRow + totalRows;
+  int endRow = startRow + totalRows;
 
-    for (int j = startRow; j < endRow; j++) {
-        for (int i = 0; i < width; ++i) {
-            float x = x0 + i * dx;
-            float y = y0 + j * dy;
+  for (int j = startRow; j < endRow; j++)
+  {
+    for (int i = 0; i < width; ++i)
+    {
+      float x = x0 + i * dx;
+      float y = y0 + j * dy;
 
-            int index = (j * width + i);
-            output[index] = mandel(x, y, maxIterations);
-        }
+      int index = (j * width + i);
+      output[index] = mandel(x, y, maxIterations);
     }
+  }
 }
-
--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -3,17 +3,17 @@
 
 #include "CycleTimer.h"
 
-typedef struct {
+typedef struct
+{
     float x0, x1;
     float y0, y1;
     unsigned int width;
     unsigned int height;
     int maxIterations;
-    int* output;
+    int *output;
     int threadId;
     int numThreads;
 } WorkerArgs;
-
 
 extern void mandelbrotSerial(
     float x0, float y0, float x1, float y1,
@@ -22,12 +22,12 @@ extern void mandelbrotSerial(
     int maxIterations,
     int output[]);
 
-
 //
 // workerThreadStart --
 //
 // Thread entrypoint.
-void workerThreadStart(WorkerArgs * const args) {
+void workerThreadStart(WorkerArgs *const args)
+{
 
     // TODO FOR CS149 STUDENTS: Implement the body of the worker
     // thread here. Each thread should make a call to mandelbrotSerial()
@@ -35,7 +35,43 @@ void workerThreadStart(WorkerArgs * const args) {
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
 
-    printf("Hello world from thread %d\n", args->threadId);
+    // printf("Hello world from thread %d\n", args->threadId);
+    // double startTime = CycleTimer::currentSeconds();
+
+    /*
+    方案1
+    // 每个线程负责的行数(除不尽的部分由最后一个线程负责)
+    int baseHeight = args->height / args->numThreads;
+    int startRow = args->threadId * baseHeight;
+    int numRows = baseHeight;
+
+    int yu = args->height % args->numThreads;
+    // 均匀分配剩余行
+    if (args->threadId < yu)
+    {
+        numRows++;
+    }
+    startRow += std::min(args->threadId, yu);
+    printf("Thread %d startRow: %d, numRows: %d\n", args->threadId, startRow, numRows);
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                     args->width, args->height,
+                     startRow, numRows,
+                     args->maxIterations, args->output);
+
+    */
+
+    // 方案2, 依次分配行
+    int height = args->height;
+    for (int i = args->threadId; i < height; i += args->numThreads)
+    {
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                         args->width, args->height,
+                         i, 1,
+                         args->maxIterations, args->output);
+    }
+
+    // double endTime = CycleTimer::currentSeconds();
+    // printf("Thread %d time: %.3f ms\n", args->threadId, (endTime - startTime) * 1000);
 }
 
 //
@@ -61,8 +97,9 @@ void mandelbrotThread(
     std::thread workers[MAX_THREADS];
     WorkerArgs args[MAX_THREADS];
 
-    for (int i=0; i<numThreads; i++) {
-      
+    for (int i = 0; i < numThreads; i++)
+    {
+
         // TODO FOR CS149 STUDENTS: You may or may not wish to modify
         // the per-thread arguments here.  The code below copies the
         // same arguments for each thread
@@ -75,22 +112,23 @@ void mandelbrotThread(
         args[i].maxIterations = maxIterations;
         args[i].numThreads = numThreads;
         args[i].output = output;
-      
+
         args[i].threadId = i;
     }
 
     // Spawn the worker threads.  Note that only numThreads-1 std::threads
     // are created and the main application thread is used as a worker
     // as well.
-    for (int i=1; i<numThreads; i++) {
+    for (int i = 1; i < numThreads; i++)
+    {
         workers[i] = std::thread(workerThreadStart, &args[i]);
     }
-    
+
     workerThreadStart(&args[0]);
 
     // join worker threads
-    for (int i=1; i<numThreads; i++) {
+    for (int i = 1; i < numThreads; i++)
+    {
         workers[i].join();
     }
 }
-
